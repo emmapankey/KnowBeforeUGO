4362ce47211ea0a91cfe10a6a3ad8d1125a50d1b
emmapankey <emmapankey19@gmail.com>
2017-11-09 19:10:29 -0500

    changed Google Map location to the city searched instead of the US Embassy and updated commdents for firebase js file

--- assets/javascript/firebase_searches.js
+++ assets/javascript/firebase_searches.js
@@ -1,50 +1,33 @@
 $(document).ready(function () {
-	//FIREBASE=========================================================
-	// Initialize Firebase
-	// var config = {
-	// 	apiKey: "AIzaSyCmj08LcW2dT_bGaFPFqNcnoID7ZRqzQJw",
-	// 	authDomain: "knowbeforugo.firebaseapp.com",
-	// 	databaseURL: "https://knowbeforugo.firebaseio.com",
-	// 	projectId: "knowbeforugo",
-	// 	storageBucket: "knowbeforugo.appspot.com",
-	// 	messagingSenderId: "172400475054"
-	// };
-	// firebase.initializeApp(config);
-	//VARIABLES=========================================================
-	var database = firebase.database();
+    //VARIABLES=========================================================
+    var database = firebase.database();
 
-	// CAPTURE BUTTON CLICK
-	$(document.body).on("click", "#submitButton", function () {
-		//VALUES FOR EACH VARIABLE IN HTML
-		var city = $('#city-input').val().trim();
-		var country = $('#country-input').val().trim();
+    // CAPTURE BUTTON CLICK
+    $(document.body).on("click", "#submitButton", function () {
+        //VALUES FOR EACH VARIABLE IN HTML
+        var city = $('#city-input').val().trim();
+        var country = $('#country-input').val().trim();
 
-		// PUSH NEW ENTRY TO FIREBASE
-		database.ref().push({
-			city: city,
-			country: country,
-			timeAdded: firebase.database.ServerValue.TIMESTAMP
-		});
-		// NO REFRESH
-		// $("input").val('');
-		// return false;
-	});
+        // PUSH NEW ENTRY TO FIREBASE
+        database.ref().push({
+            city: city,
+            country: country,
+            timeAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+    });
 
-	//ON CLICK CHILD FUNCTION
-	database.ref().on("child_added", function (childSnapshot) {
+    //ON CLICK CHILD FUNCTION
+    database.ref().on("child_added", function (childSnapshot) {
 
-		var city = childSnapshot.val().city;
-		var country = childSnapshot.val().country;
-		// var timeAdded = childSnapshot.val().timeAdded;
-		// var Enteredtime = moment(childSnapshot.val().timeAdded, 'HH:mm');
+        var city = childSnapshot.val().city;
+        var country = childSnapshot.val().country;
 
-		//TABLE DATA=====================================================
-		
-		$('#searches-table').append(
-			"<tr><td>" + childSnapshot.val().city +
-			"</td><td>" + childSnapshot.val().country +
-			"</td></tr>");
+        //TABLE DATA=====================================================
+        $('#searches-table').append(
+            "<tr><td>" + childSnapshot.val().city +
+            "</td><td>" + childSnapshot.val().country +
+            "</td></tr>");
 
-	});
+    });
 
 }); //END DOCUMENT.READY
