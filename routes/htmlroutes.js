1cb10e07f5fa2d5c4f28220c171f9e568e2b141d
emmapankey <emmapankey19@gmail.com>
2018-03-23 11:41:23 -0400

    Implemented a node server

---
+++ routes/htmlroutes.js
@@ -0,0 +1,13 @@
+// Dependencies
+// The path npm package is needed to get the correct file paths for the html pages
+var path = require("path");
+
+// Routes
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  app.get("/savedsearches", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/saved-searches.html"));
+  });
+};
